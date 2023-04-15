33e73e89b04f6751afde470098d677151122cae3
Christos Balaktsis <mpalaktsc@csd.auth.gr>
2023-04-15 20:57:09 +0300

    Initialized project

---
+++ app/src/main/java/com/auth/studywithme/MainActivity.java
@@ -0,0 +1,16 @@
+package com.auth.studywithme;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.auth.studywithme.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
