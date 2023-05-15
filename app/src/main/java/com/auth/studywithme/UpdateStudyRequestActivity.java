c901faf0f53a6dad13b26da59fe4d4085c740855
Stratos <anthopoue@csd.auth.gr>
2023-05-15 15:41:18 +0300

    update .gitignore

---
+++ app/src/main/java/com/auth/studywithme/UpdateStudyRequestActivity.java
@@ -0,0 +1,14 @@
+package com.auth.studywithme;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class UpdateStudyRequestActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_update_study_request);
+    }
+}
