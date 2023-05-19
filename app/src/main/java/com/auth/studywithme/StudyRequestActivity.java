c1ccdbe746c98d3c8e1d7e360b769fc0c4b15536
Stratos <anthopoue@csd.auth.gr>
2023-05-20 00:42:34 +0300

    add comments and maxMatches fields and period of study dropdown

--- app/src/main/java/com/auth/studywithme/StudyRequestActivity.java
+++ app/src/main/java/com/auth/studywithme/StudyRequestActivity.java
@@ -4,7 +4,10 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.EditText;
+import android.widget.Spinner;
 
 import java.util.Date;
 
@@ -12,10 +15,13 @@ public class StudyRequestActivity extends AppCompatActivity {
 
     private User loggedUser;
     StorageHandler storageHandler;
-
     private EditText subjectEditText;
     private EditText reasonEditText;
     private EditText placeEditText;
+    private EditText commentsEditText;
+    private EditText maxMatchesEditText;
+    private PeriodOfStudy selectedPeriod;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,15 +41,59 @@ public class StudyRequestActivity extends AppCompatActivity {
         subjectEditText = findViewById(R.id.et_subject);
         reasonEditText = findViewById(R.id.et_reason);
         placeEditText = findViewById(R.id.et_place);
+        commentsEditText = findViewById(R.id.et_comments);
+        maxMatchesEditText = findViewById(R.id.et_max_matches);
+
+        Spinner spinnerPeriod = findViewById(R.id.sp_period);
+        // Define the string array for the spinner values
+        String[] periodValues = new String[]{
+                "once",
+                "twice",
+                "sometimes",
+                "one_week",
+                "two_weeks",
+                "semester",
+                "other"
+        };
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(
+                this,
+                android.R.layout.simple_spinner_item,
+                periodValues
+        );
+
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinnerPeriod.setAdapter(adapter);
+
+        // Set listener so the spinner can listen for the selected item
+        spinnerPeriod.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                String selectedItem = parent.getItemAtPosition(position).toString();
+                // Convert the selected string value to the corresponding PeriodOfStudy enum value
+                selectedPeriod = PeriodOfStudy.valueOf(selectedItem);
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+                // Handle case when no item is selected (if needed)
+                selectedPeriod = null;
+            }
+        });
+
 
     }
 
     public void onBtnClick(View view) {
+        // Get form values
         String subject = subjectEditText.getText().toString();
         String reason = reasonEditText.getText().toString();
         String place = placeEditText.getText().toString();
+        String comments = commentsEditText.getText().toString();
+        String maxMatches = maxMatchesEditText.getText().toString();
 
-        StudyRequest sr = new StudyRequest(subject, reason, place,"o", new Date(),PeriodOfStudy.once,4);
+        // Create new Study Request object and add it to the database
+        StudyRequest sr = new StudyRequest(subject, reason, place,comments, new Date(),selectedPeriod,Integer.parseInt(maxMatches));
         storageHandler.addStudyRequest(sr,loggedUser);
 
         setResult(100);
--- app/src/main/java/com/auth/studywithme/UpdateStudyRequestActivity.java
+++ app/src/main/java/com/auth/studywithme/UpdateStudyRequestActivity.java
@@ -2,6 +2,7 @@ package com.auth.studywithme;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
@@ -10,11 +11,14 @@ import java.util.ArrayList;
 
 public class UpdateStudyRequestActivity extends AppCompatActivity {
     private StudyRequest studyRequest;
-
     private EditText subjectEditText;
     private EditText reasonEditText;
     private EditText placeEditText;
+    private EditText commentsEditText;
+    private EditText maxMatchesEditText;
 
+
+    @SuppressLint("SetTextI18n")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -29,11 +33,15 @@ public class UpdateStudyRequestActivity extends AppCompatActivity {
         subjectEditText = findViewById(R.id.et_subject);
         reasonEditText = findViewById(R.id.et_reason);
         placeEditText = findViewById(R.id.et_place);
+        commentsEditText = findViewById(R.id.et_comments);
+        maxMatchesEditText = findViewById((R.id.et_max_matches));
 
         // Pre-fill the form fields with the current values of the study request
         subjectEditText.setText(studyRequest.getSubject());
         reasonEditText.setText(studyRequest.getReason());
         placeEditText.setText(studyRequest.getPlace());
+        commentsEditText.setText(studyRequest.getComments());
+        maxMatchesEditText.setText(Integer.toString(studyRequest.getMaxMatches()));
         } else {
             finish();
         }
@@ -44,11 +52,15 @@ public class UpdateStudyRequestActivity extends AppCompatActivity {
         String updatedSubject = subjectEditText.getText().toString();
         String updatedReason = reasonEditText.getText().toString();
         String updatedPlace = placeEditText.getText().toString();
+        String updatedComments = commentsEditText.getText().toString();
+        String updatedMaxMatches = maxMatchesEditText.getText().toString();
 
         // Update the study request object with the new values
         studyRequest.setSubject(updatedSubject);
         studyRequest.setReason(updatedReason);
         studyRequest.setPlace(updatedPlace);
+        studyRequest.setComments(updatedComments);
+        studyRequest.setMaxMatches(Integer.parseInt(updatedMaxMatches));
 
         // Save the updated study request to the database
         try (StorageHandler sh = new StorageHandler(this, null, 1)) {
