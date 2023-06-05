8cc07e16b437ba41aeaf6f877d38fa929cb54b46
Stratos <anthopoue@csd.auth.gr>
2023-06-05 14:07:13 +0300

    add documentation comments to the MatchesListActivity

--- app/src/main/java/com/auth/studywithme/MatchesListActivity.java
+++ app/src/main/java/com/auth/studywithme/MatchesListActivity.java
@@ -14,6 +14,10 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import java.util.Objects;
 
+/**
+ * The MatchesListActivity displays a list of study request matches for a particular study request.
+ * It allows the user to view the details of each match and navigate back to the previous activity.
+ */
 public class MatchesListActivity extends AppCompatActivity implements RecyclerAdapter.ISStudyRequestRecycler {
     public static double SIMILARITY_THRESHOLD = 0.6;
     RecyclerView recyclerView;
@@ -25,36 +29,44 @@ public class MatchesListActivity extends AppCompatActivity implements RecyclerAd
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_matches_list);
+
+        // Set the default night mode to NO night mode
         AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+
+        // Enable the back button in the action bar for navigating back
         Objects.requireNonNull(getSupportActionBar()).setDisplayHomeAsUpEnabled(true);
 
-        sh = new StorageHandler(this,null,1);
+        // Initialize the StorageHandler
+        sh = new StorageHandler(this, null, 1);
 
+        // Retrieve the study request ID passed from the previous activity
         Bundle extras = getIntent().getExtras();
         if (extras != null)
             sr = sh.fetchStudyRequestById(extras.getLong("studyRequestId"));
         else
             finish();
 
+        // Set up the RecyclerView to display the list of matches
         recyclerView = findViewById(R.id.recycler_view);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
-        adapter = new RecyclerAdapter(this,sr,this,sh);
+        adapter = new RecyclerAdapter(this, sr, this, sh);
         recyclerView.setAdapter(adapter);
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle the selection of menu items in the action bar
         if (item.getItemId() == android.R.id.home) {
+            // When the back button is pressed, navigate back to the previous activity
             onBackPressed();
             return true;
         }
         return super.onOptionsItemSelected(item);
     }
 
-
     @Override
     public void showStudyRequestDetails(long srId) {
-        // Start the UpdateStudyRequestActivity and pass the selected study request
+        // Start the ViewStudyRequestActivity and pass the selected study request ID and the ID of the requested user
         Intent intent = new Intent(this, ViewStudyRequestActivity.class);
         intent.putExtra("studyRequestId", srId);
         intent.putExtra("loggedUserId", sr.getRequestedUserId());
