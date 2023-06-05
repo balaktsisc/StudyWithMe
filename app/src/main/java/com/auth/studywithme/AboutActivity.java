e9a0a2222e21bd0ed6d98901ef1107979230dac6
Stratos <anthopoue@csd.auth.gr>
2023-06-05 13:23:43 +0300

    add comments in AboutActivity

--- app/src/main/java/com/auth/studywithme/AboutActivity.java
+++ app/src/main/java/com/auth/studywithme/AboutActivity.java
@@ -1,10 +1,8 @@
 package com.auth.studywithme;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 import android.view.MenuItem;
-
 import java.util.Objects;
 
 public class AboutActivity extends AppCompatActivity {
@@ -13,17 +11,19 @@ public class AboutActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
-        Objects.requireNonNull(getSupportActionBar()).setDisplayHomeAsUpEnabled(true);
 
+        // Set up the back button in the action bar
+        Objects.requireNonNull(getSupportActionBar()).setDisplayHomeAsUpEnabled(true);
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle menu item selection
         if (item.getItemId() == android.R.id.home) {
+            // If the home button is pressed, go back to the previous activity
             onBackPressed();
             return true;
         }
         return super.onOptionsItemSelected(item);
     }
-
 }
