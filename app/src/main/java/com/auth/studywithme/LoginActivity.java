342b02884f8144647ca9546f2fcf9a24d8b8e1c0
Stratos <anthopoue@csd.auth.gr>
2023-06-05 14:02:18 +0300

    add documentation comments to the LoginActivity

--- app/src/main/java/com/auth/studywithme/LoginActivity.java
+++ app/src/main/java/com/auth/studywithme/LoginActivity.java
@@ -16,6 +16,10 @@ import android.widget.Toast;
 
 import java.util.Objects;
 
+/**
+ * Activity for user login. Allows users to enter their username and password to authenticate and log in.
+ * Provides an option to sign up for a new account.
+ */
 public class LoginActivity extends AppCompatActivity {
     EditText usernameEditText;
     EditText passwordEditText;
@@ -27,37 +31,43 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+        // Set the app's default night mode to NO night mode
         AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+        // Hide the action bar
         Objects.requireNonNull(getSupportActionBar()).hide();
 
         // Get references to the username and password EditText views
         usernameEditText = findViewById(R.id.editTxtUsername);
         passwordEditText = findViewById(R.id.editTxtPassword);
 
-        // Initialize StorageHandler
+        // Initialize the StorageHandler
         storageHandler = new StorageHandler(this,null,1);
 
-
+        // Set up the "Not a member? Sign up now" text view with clickable span
         TextView textView = findViewById(R.id.textViewSignUp);
         SpannableString spannableString = new SpannableString("Not a member? Sign up now");
-
         textView.setText(spannableString);
 
         ClickableSpan clickableSpan = new ClickableSpan() {
             @Override
             public void onClick(View view) {
+                // Start the SignUpActivity when the user clicks the span
                 Intent intent = new Intent(LoginActivity.this, SignUpActivity.class);
                 startActivity(intent);
             }
         };
 
         spannableString.setSpan(clickableSpan, 14, 25, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
-
         textView.setText(spannableString);
         textView.setMovementMethod(LinkMovementMethod.getInstance());
     }
 
-    public void LoginClicked (View v){
+    /**
+     * Called when the "Login" button is clicked. Attempts to authenticate the user with the entered credentials.
+     *
+     * @param v The view that was clicked (the "Login" button)
+     */
+    public void LoginClicked(View v) {
         // Get the username and password entered by the user
         String username = usernameEditText.getText().toString();
         String password = passwordEditText.getText().toString();
@@ -68,11 +78,10 @@ public class LoginActivity extends AppCompatActivity {
             // Show a success message
             Toast.makeText(LoginActivity.this, "Logged in successfully", Toast.LENGTH_SHORT).show();
 
-            // Switch to the new activity for creating a study request
+            // Switch to the DashboardActivity for the logged-in user
             Intent intent = new Intent(this, DashboardActivity.class);
             intent.putExtra("loggedUserId",loggedUser.getId());
             startActivity(intent);
-//            finish();
         } else {
             // Show an error message
             Toast.makeText(LoginActivity.this, "Invalid username or password", Toast.LENGTH_SHORT).show();
@@ -84,12 +93,19 @@ public class LoginActivity extends AppCompatActivity {
         super.onResume();
         usernameEditText = findViewById(R.id.editTxtUsername);
         passwordEditText = findViewById(R.id.editTxtPassword);
+        // Clear the password field when the activity resumes
         passwordEditText.setText("");
     }
 
-    // Check if the username and password are valid
+    /**
+     * Authenticate the user with the provided username and password.
+     *
+     * @param username The username entered by the user
+     * @param password The password entered by the user
+     * @return The authenticated User object, or null if authentication fails
+     */
     private User authenticateUser(String username, String password){
-        // Search database for user with specified username and password
+        // Search the database for a user with the specified username and password
         return storageHandler.fetchUserByCredentials(username, password);
     }
 }
